f3d89bc80ea73f284224ae2f7f35f45cc02e9f89
wasita <wasita.mahaphanit.gr@dartmouth.edu>
2026-03-01 16:57:03 -0500

    🎨 soften light mode palette, gradient page title, lowercase breadcrumbs

--- quartz/components/Breadcrumbs.tsx
+++ quartz/components/Breadcrumbs.tsx
@@ -30,7 +30,7 @@ interface BreadcrumbOptions {
 
 const defaultOptions: BreadcrumbOptions = {
   spacerSymbol: "❯",
-  rootName: "Home",
+  rootName: "home",
   resolveFrontmatterTitle: true,
   showCurrentPage: true,
 }
