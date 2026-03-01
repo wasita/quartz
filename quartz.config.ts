43d19ac23600fc6c85e53535216ecd3a7705fd51
wasita <wasita.mahaphanit.gr@dartmouth.edu>
2026-03-01 17:08:41 -0500

    🚀 add GitHub Actions deploy workflow for wasita.github.io/quartz

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "wasita.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
